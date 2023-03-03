0ab9c5a7b7142db79b1d3e1caf9cd535b8ca14dc
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-03-02 22:16:42 -0300

    finish first part

---
+++ react/01-github-explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,9 @@
+function RepositoryItem({ repository }) {
+    return <>
+        <h1>{repository.name}</h1>
+        <h3>{repository.description}</h3>
+        <a href={repository.html_url}>Click here</a>
+    </>;
+}
+
+export default RepositoryItem;

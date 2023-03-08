c83df241c8cdb8972483c7d279151054e3d478e8
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-03-08 18:01:18 -0300

    finish challenge 1

---
+++ react/challenge1/src/App.tsx
@@ -0,0 +1,12 @@
+import { TaskList } from "./components/TaskList";
+import { Header } from "./components/Header";
+import "./styles/global.css";
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <TaskList />
+        </>
+    );
+}
---
+++ react/challenge1/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "../styles/header.css";
+
+export function Header() {
+    return (
+        <header className="header">
+            <div>
+                <h2>To Do</h2>
+            </div>
+        </header>
+    );
+}

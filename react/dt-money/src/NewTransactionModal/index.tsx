ae253bc0d5ea791767bdc47e4497b2cd7798e3df
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-03-16 23:56:35 -0300

    api connection

--- react/dt-money/src/NewTransactionModal/index.tsx
+++ react/dt-money/src/NewTransactionModal/index.tsx
@@ -1,5 +1,11 @@
+import { useState, FormEvent } from "react";
+import { api } from "../sevices/api";
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer, TypeButton } from "./styles";
+import closeImg from "../assets/fechar.svg";
+import incomeImg from "../assets/entradas.svg";
+import outcomeImg from "../assets/saidas.svg";
+
 Modal.setAppElement("#root");
 
 interface NewTransactionModalProps {
@@ -11,10 +17,81 @@ export function NewTransactionModal({
     isOpen,
     onRequestClose,
 }: NewTransactionModalProps) {
+    const [title, setTitle] = useState("");
+    const [amount, setAmount] = useState(0);
+    const [category, setCategory] = useState("");
+    const [type, setType] = useState("deposit");
+
+    function handleNewTransaction(e: FormEvent) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            amount,
+            category,
+            type,
+        };
+
+        api.post("/transactions", data);
+    }
+
     return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-            <Container>
-                <h1>oi</h1>
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <button>
+                <img
+                    src={closeImg}
+                    alt="Fechar modal"
+                    onClick={onRequestClose}
+                    className="react-modal-close"
+                />
+            </button>
+
+            <Container onSubmit={handleNewTransaction}>
+                <h2>Cadastrar transação</h2>
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <input
+                    type="number"
+                    placeholder="Preço"
+                    value={amount}
+                    onChange={(e) => setAmount(Number(e.target.value))}
+                />
+                <TransactionTypeContainer>
+                    <TypeButton
+                        type="button"
+                        isActive={type === "deposit"}
+                        onClick={() => setType("deposit")}
+                        activeColor="green"
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </TypeButton>
+                    <TypeButton
+                        type="button"
+                        isActive={type === "withdraw"}
+                        onClick={() => setType("withdraw")}
+                        activeColor="red"
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </TypeButton>
+                </TransactionTypeContainer>
+                <input
+                    type="text"
+                    placeholder="Categoria"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                />
+                <button type="submit">Cadastrar</button>
             </Container>
         </Modal>
     );
--- react/dt-money/src/styles/global.ts
+++ react/dt-money/src/styles/global.ts
@@ -13,6 +13,9 @@ export const GlobalStyle = createGlobalStyle`
         --text-body: #969CB3;
 
         --shape: #FFFFFF;
+
+        --input-background: #e7e9ee;
+        --input-border: #d7d7d7;
     }
 
     html {
@@ -52,5 +55,43 @@ export const GlobalStyle = createGlobalStyle`
     [disabled] {
         cursor: not-allowed;
         opacity: 0.6;
+    }
+
+    .react-modal-overlay {
+        background-color: rgba(0,0,0,0.5);
+
+        position: fixed;
+        top:0;
+        bottom:0;
+        left:0;
+        right:0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.25rem;
+    }
+
+    .react-modal-close {
+         position: absolute;
+         right: 1.5rem;
+         top: 1.5rem;
+
+        transition: filter 0.2s;
+
+        :hover {
+            filter: brightness(0.9);
+        }
+
+         /* border: none; */
+         /* background: transparent */
     }
 `;

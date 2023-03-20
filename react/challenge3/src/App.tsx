35633b6551376afbb43e06ffe06c706a2f65ff97
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-03-20 14:01:51 -0300

    imported challenge3 files

--- react/challenge3/src/App.tsx
+++ react/challenge3/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import { CartProvider } from './hooks/useCart';
 
+const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <CartProvider>
+        <GlobalStyles />
+        <Header />
+        <Routes />
+        <ToastContainer autoClose={3000} />
+      </CartProvider>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;

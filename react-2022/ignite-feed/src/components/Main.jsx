91c90a6c8df5d7290c1a4687885ba595bbc89fc9
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-04-20 00:18:24 -0300

    header and side bar done

---
+++ react-2022/ignite-feed/src/components/Main.jsx
@@ -0,0 +1,10 @@
+import styles from './Main.module.css';
+
+export default function Main() {
+  return (
+  <main className={styles.main}>
+        <h1>oi</h1>
+        <h1>oi</h1>
+    </main>
+  )
+}
